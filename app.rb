a1504ffc56ed9e1f605c2a55aaee2c0346851fcb
katcarr <kathrynelizabethcarr@gmail.com>
2015-01-30 11:54:28 -0800

    Add web functionality to add,list and validate venues on home page

--- app.rb
+++ app.rb
@@ -1,3 +1,19 @@
 require("bundler/setup")
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + "/lib/*.rb"].each { |file| require file}
+
+
+get("/") do
+  @venues = Venue.all()
+  erb(:index)
+end
+
+post("/add_venue") do
+  name = params.fetch("name")
+  @venue = Venue.new({:name => name})
+  if @venue.save()
+    redirect '/'
+  else
+    erb(:errors)
+  end
+end
