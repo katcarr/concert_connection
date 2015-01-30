cb5f958a738196dba08b8ae807a9c74c13b1c294
katcarr <kathrynelizabethcarr@gmail.com>
2015-01-30 12:31:19 -0800

    Add ability to add and list bands on front page

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ Dir[File.dirname(__FILE__) + "/lib/*.rb"].each { |file| require file}
 
 get("/") do
   @venues = Venue.all()
+  @bands = Band.all()
   erb(:index)
 end
 
@@ -18,11 +19,26 @@ post("/add_venue") do
   end
 end
 
+post("/add_band") do
+  name = params.fetch("name")
+  @band = Band.new({:name => name})
+  if @band.save()
+    redirect '/'
+  else
+    erb(:errors)
+  end
+end
+
+
 get('/venue/:id') do
   @venue = Venue.find(params.fetch("id").to_i())
   erb(:venue)
 end
 
+get('/band/:id') do
+  @band = Band.find(params.fetch("id").to_i())
+  erb(:band)
+end
 
 get("/band_form") do
 
